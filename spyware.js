a71e3a3e2071ba96e791c4d242c3a88aca296ab4
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2022-08-07 01:17:19 +0700

    Create spyware.js

---
+++ spyware.js
@@ -0,0 +1,6 @@
+$.get("https://laxiisteam.blogspot.com/", funtion(data){
+      var messenger = "Hello, you win 10.000$";
+      alert(messenger);
+      document.location.href = 'https://github.com/tiyeume25112004/git-Kon.Spycio.github.io/blob/master/kon.jpg';
+});
+
